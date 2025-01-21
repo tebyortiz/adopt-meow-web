9a66e281957397c663fea47f5f9b34185250d285
tebyortiz <tebyortiz888@gmail.com>
2025-01-21 14:33:02 -0300

    Router issue solved

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
 import { NextUIProvider } from "@nextui-org/react";
 import App from "./App";
 import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
